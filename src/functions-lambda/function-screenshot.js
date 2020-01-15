fd1d393c6df0d91c159098de62a84325784bfa7c
Jerome <bouchon.jerome@gmail.com>
2020-01-15 15:37:55 -0800

    Updating screenshot function to allow netlify deployment

--- src/functions-lambda/function-screenshot.js
+++ src/functions-lambda/function-screenshot.js
@@ -1,6 +1,60 @@
 const captureWebsite = require('capture-website');
+const chromium = require('chrome-aws-lambda');
 
 exports.handler = async event => {
+  console.log(`event.body`);
+  console.log(event.body);
+
+  if (true) {
+    // const eventPageToScreenshot = JSON.parse(event.body).pageToScreenshot;
+    const pageToScreenshot = 'https://starry-poster.netlify.com/?posterPrint=1';
+
+    if (!pageToScreenshot)
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'Page URL not defined' }),
+      };
+
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(pageToScreenshot, { waitUntil: 'networkidle2' });
+
+    const screenshot = await page.screenshot({
+      fullPage: true,
+      quality: 100,
+      type: 'jpeg',
+      encoding: 'base64',
+    });
+
+    await browser.close();
+
+    // return {
+    //   statusCode: 200,
+    //   headers: {
+    //     'Content-type': 'image/jpeg',
+    //   },
+    //   body: JSON.stringify({
+    //     message: `Complete screenshot of ${pageToScreenshot}`,
+    //     buffer: screenshot,
+    //   }),
+    // };
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-type': 'image/jpeg',
+      },
+      body: screenshot,
+      isBase64Encoded: true,
+    };
+  }
   const printBool = event.queryStringParameters.posterPrint || '0';
   const design = event.queryStringParameters.posterDesign || '3';
 
