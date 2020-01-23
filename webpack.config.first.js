2ef736c58f20b8b4626b0fd850e87dce711d596a
Jerome <bouchon.jerome@gmail.com>
2020-01-23 04:44:43 -0800

    Adding icons

--- webpack.config.first.js
+++ webpack.config.first.js
@@ -26,7 +26,7 @@ const htmlTemplate = isProduction => `
 
               <link href="${fontsCustom}" rel="stylesheet">
               <link rel="canonical" href="${canonical}" />
-              <link rel="apple-touch-icon" href="/icon_192x192.b85f732afd047b469bc878c77e2503af.png">
+              <link rel="apple-touch-icon" href="app/assets/icons/icon_192x192.png">
 
               <title>${appName}</title>
               <meta charset="UTF-8">
